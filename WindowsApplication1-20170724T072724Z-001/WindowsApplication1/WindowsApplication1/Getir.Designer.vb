c01a3dac2e0892ebe016620c58e6195c68ac6e04
Pubs <minecraftgamererdem@gmail.com>
2018-04-28 17:41:42 +0300

    Başlık
    Cidiyim azcık espiriliyim işte bü yüzden teammimle çaılışıyoruz

---
+++ WindowsApplication1-20170724T072724Z-001/WindowsApplication1/WindowsApplication1/Getir.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Getir
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Yemek", "İçecek", "Aburcubur", "Bilgisayar", "Telefon"})
+        Me.ComboBox1.Location = New System.Drawing.Point(485, 58)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(177, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(485, 107)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(177, 21)
+        Me.ComboBox2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(482, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Malzeme"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(89, 296)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Adet"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(92, 335)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(64, 17)
+        Me.CheckBox1.TabIndex = 4
+        Me.CheckBox1.Text = "2 Taksit"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(92, 382)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(64, 17)
+        Me.CheckBox2.TabIndex = 5
+        Me.CheckBox2.Text = "4 Taksit"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(213, 335)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(64, 17)
+        Me.CheckBox3.TabIndex = 6
+        Me.CheckBox3.Text = "3 Taksit"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Location = New System.Drawing.Point(213, 382)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(64, 17)
+        Me.CheckBox4.TabIndex = 7
+        Me.CheckBox4.Text = "5 Taksit"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'Getir
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(866, 532)
+        Me.Controls.Add(Me.CheckBox4)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Getir"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+End Class
---
+++ WindowsApplication1-20170724T072724Z-001/WindowsApplication1/WindowsApplication1/Getir.vb
@@ -0,0 +1,110 @@
+﻿Public Class Getir
+    Dim Döner, Pizza, Hamburger, Pilav, Köfte, Patetes, Patlıcan, Yoğurt, Tavuk, Et, Sucuk, Ekmek, İcetea, Kola, Pepsi, Gazoz, Ayran, Meyve, Su, Süt, Hoşbeş, kat, pop, ülker, Piko, Crax, Nutela, konflex, Mac, Windows, WindowsP, WindowsPR, WindowsPRO
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        ComboBox2.Items.Clear()
+
+        If ComboBox1.Text = "Yemek" Then
+
+            ComboBox2.Items.Add("Döner")
+
+            ComboBox2.Items.Add("Pizza")
+
+            ComboBox2.Items.Add("Hamburger")
+
+            ComboBox2.Items.Add("Pilav")
+
+            ComboBox2.Items.Add("Köfte")
+
+            ComboBox2.Items.Add("Patates")
+
+            ComboBox2.Items.Add("Patlıcan")
+
+            ComboBox2.Items.Add("Yoğurt")
+
+            ComboBox2.Items.Add("Tavuk")
+
+            ComboBox2.Items.Add("Et")
+
+            ComboBox2.Items.Add("Sucuk")
+
+            ComboBox2.Items.Add("Ekmek")
+
+        ElseIf ComboBox1.Text = "İçicek" Then
+
+            ComboBox2.Items.Add("İcetea")
+
+            ComboBox2.Items.Add("Kola")
+
+            ComboBox2.Items.Add("Pepsi")
+
+            ComboBox2.Items.Add("Gazoz")
+
+            ComboBox2.Items.Add("Ayran")
+
+            ComboBox2.Items.Add("Meyve Suy")
+
+            ComboBox2.Items.Add("Su")
+
+            ComboBox2.Items.Add("Süt")
+
+        ElseIf ComboBox1.Text = "Aburcubur" Then
+
+            ComboBox2.Items.Add("Hoşbeş")
+
+            ComboBox2.Items.Add("9 kat")
+
+            ComboBox2.Items.Add("pop kek")
+
+            ComboBox2.Items.Add("Ülker çikolata")
+
+            ComboBox2.Items.Add("Piko")
+
+            ComboBox2.Items.Add("Crax")
+
+            ComboBox2.Items.Add("Nutela")
+
+            ComboBox2.Items.Add("konflex")
+
+        ElseIf ComboBox1.Text = "Bilgisayar" Then
+
+            ComboBox2.Items.Add("Mac")
+
+            ComboBox2.Items.Add("Windows 7 Pro")
+
+            ComboBox2.Items.Add("Windows 10")
+
+            ComboBox2.Items.Add("Windows 10 Pro")
+
+            ComboBox2.Items.Add("Windows 8")
+
+        ElseIf ComboBox1.Text = "Telefon" Then
+
+            ComboBox2.Items.Add("Iphone 5s")
+
+            ComboBox2.Items.Add("Iphone 6")
+
+            ComboBox2.Items.Add("Iphone 7")
+
+            ComboBox2.Items.Add("Iphone 7 plus")
+
+            ComboBox2.Items.Add("Note 3")
+
+
+
+            ComboBox2.Items.Add("Note 4")
+
+            ComboBox2.Items.Add("Samsung")
+
+            ComboBox2.Items.Add("Lg")
+
+
+
+
+
+
+
+
+        End If
+
+    End Sub
+End Class
